a8de27c3c6fb1c8ef2db34f00f48c2741a3ab5eb
GAVRIL SAMODIVEKOV <gsamodivekov@gmail.com>
2023-02-04 22:09:36 +0200

    Added ClearEmailsCommand.

--- src/com/mail/commands/models/ClearEmailsCommand.java
+++ src/com/mail/commands/models/ClearEmailsCommand.java
@@ -1,17 +1,78 @@
 package com.mail.commands.models;
 
 import com.mail.commands.repository.TempState;
+import com.mail.factory.constructors.PopConstructorImpl;
+import com.mail.factory.constructors.PopConstructorInterface;
+import com.mail.factory.models.CheckedEmail;
+import com.mail.factory.models.EmailServerConnector;
+import com.mail.factory.models.PopClientInterface;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.List;
 
 public class ClearEmailsCommand implements CommandInterface {
-    private boolean sendSummary;
+    private boolean clearEmails;
 
 
-    public ClearEmailsCommand(boolean sendSummary) {
-        setSendSummary(sendSummary);
+    public ClearEmailsCommand(boolean clearEmails) {
+        setClearEmails(clearEmails);
     }
 
     public void executeCommand(TempState state) {
+        List<CheckedEmail> emails;
+        EmailServerConnector connector = chooseConnector(state.getCurrentConnectors());
+        if (connector == null) {
+            System.out.println("Something went wrong! Please try again...");
+        }
 
+        PopConstructorInterface popConstructor = new PopConstructorImpl();
+        PopClientInterface popClient = popConstructor.returnPopClient(connector);
+        int messageCountToClear = getMessageCountToClear();
+        emails = popClient.retrieveAndClean(messageCountToClear, this.clearEmails);
+
+        if (emails == null) {
+            System.out.println("Something went wrong! Please try again...");
+        }
+        for (CheckedEmail elem: emails) {
+            state.getCurrentEmails().add(elem);
+        }
+        if (this.clearEmails) {
+            System.out.println("Emails retrieved and cleared successfully!");
+        } else {
+            System.out.println("Emails retrieved successfully!");
+        }
+    }
+
+
+    private EmailServerConnector chooseConnector(List<EmailServerConnector> connectors) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Available connectors:");
+        for (int i=0; i<connectors.size(); i++) {
+            System.out.println(i + ". " + connectors.get(i).toString());
+        }
+        System.out.println("Choose which connector to use:");
+        try {
+            int conId = Integer.parseInt(reader.readLine());
+            return connectors.get(conId);
+        } catch (Exception e) {
+            System.out.println("Something went wrong! Please try again...");
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private int getMessageCountToClear() {
+        int messageCount = 0;
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Please specify how many messages do you want to clear:");
+            messageCount = Integer.parseInt(reader.readLine());
+        } catch (Exception e) {
+            System.out.println("Something went wrong! Please try again...");
+            e.printStackTrace();
+        }
+        return messageCount;
     }
 
 
@@ -19,11 +80,11 @@ public class ClearEmailsCommand implements CommandInterface {
 
     }
 
-    private void setSendSummary(boolean sendSummary) {
-        this.sendSummary = sendSummary;
+    private void setClearEmails(boolean clearEmails) {
+        this.clearEmails = clearEmails;
     }
 
-    private boolean getSendSummary() {
-        return this.sendSummary;
+    private boolean getClearEmails() {
+        return this.clearEmails;
     }
 }
